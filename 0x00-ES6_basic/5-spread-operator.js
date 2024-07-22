71d92005aef22a3edc4fc10d9a9976a73ba0773c
creih <iherr.9@gmail.com>
2024-07-22 22:18:06 +0200

    task 5 added comment

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
+// This function takes in three parameters: array1, array2, and string.
 export default function concatArrays(array1, array2, string) {
     return [...array1, ...array2, ...string];
   }
