1da579fbd8dca78c10f0f4021e6794b5c4172fd9
creih <iherr.9@gmail.com>
2024-07-25 12:48:16 +0200

    task 7 done for airport class

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,23 +1,7 @@
 export default class Airport {
     constructor(name, code) {
-      this._name = this._validateName(name);
-      this._code = this._validateCode(code);
-    }
-  
-    // Validate name
-    _validateName(name) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      return name;
-    }
-  
-    // Validate code
-    _validateCode(code) {
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      return code;
+      this._name = name;
+      this._code = code;
     }
   
     // Getter for name
@@ -27,7 +11,7 @@ export default class Airport {
   
     // Setter for name
     set name(newName) {
-      this._name = this._validateName(newName);
+      this._name = newName;
     }
   
     // Getter for code
@@ -37,7 +21,7 @@ export default class Airport {
   
     // Setter for code
     set code(newCode) {
-      this._code = this._validateCode(newCode);
+      this._code = newCode;
     }
   
     // Override toString method to return the airport code
