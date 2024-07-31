d25f2b2b1cbf3eab749940861f42a03c5f4c4c52
creih <iherr.9@gmail.com>
2024-07-31 21:55:44 +0000

    correction of file where the tripple slashes were ommited

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,25 +1,30 @@
-<reference path="subjects/Teacher.ts" />
-<reference path="subjects/Subject.ts" />
-<reference path="subjects/Cpp.ts" />
-<reference path="subjects/React.ts" />
-<reference path="subjects/Java.ts" />
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/React.ts" />
+/// <reference path="subjects/Java.ts" />
 
-const cpp = new Subjects.Cpp(new Subjects.Teacher('John', 'Doe'));
-const java = new Subjects.Java(new Subjects.Teacher('Jane', 'Smith'));
-const react = new Subjects.React(new Subjects.Teacher('James', 'Brown'));
+// Create and export constants for each subject
+export const cpp = new Subjects.Cpp(new Subjects.Teacher('John', 'Doe'));
+export const java = new Subjects.Java(new Subjects.Teacher('Jane', 'Smith'));
+export const react = new Subjects.React(new Subjects.Teacher('James', 'Brown'));
 
-const cTeacher = { firstName: 'Guillaume', lastName: 'Salva', experienceTeachingC: 10 };
+// Create and export a Teacher object with experienceTeachingC
+export const cTeacher: Subjects.Teacher = { firstName: 'Guillaume', lastName: 'Salva', experienceTeachingC: 10 };
 
+// Test the Cpp subject
 console.log('C++');
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
+// Test the Java subject
 console.log('Java');
 java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
+// Test the React subject
 console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
