7f95caf449c466e79e14ac9a9e4e182be26f0f1e
creih <iherr.9@gmail.com>
2024-07-31 21:32:35 +0000

    creation of tecclass function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,3 +59,17 @@ function executeWork(employee: Director | Teacher): void {
         console.log(employee.workTeacherTasks());
     }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    } else {
+        throw new Error('Invalid subject');
+    }
+}
+
+
