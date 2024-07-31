76ab5785aa32a07c86df007aaa6e686b00550fac
creih <iherr.9@gmail.com>
2024-07-31 21:47:35 +0000

    made necessary file for task 9 to create n implement namespaces n classes eith declaration merging

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,26 @@
+<reference path="subjects/Teacher.ts" />
+<reference path="subjects/Subject.ts" />
+<reference path="subjects/Cpp.ts" />
+<reference path="subjects/React.ts" />
+<reference path="subjects/Java.ts" />
+
+const cpp = new Subjects.Cpp(new Subjects.Teacher('John', 'Doe'));
+const java = new Subjects.Java(new Subjects.Teacher('Jane', 'Smith'));
+const react = new Subjects.React(new Subjects.Teacher('James', 'Brown'));
+
+const cTeacher = { firstName: 'Guillaume', lastName: 'Salva', experienceTeachingC: 10 };
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
